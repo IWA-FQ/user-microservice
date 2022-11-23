054c2c5f2a2bf28e6727ecdb14acb6ae79703108
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-11-23 10:07:59 +0100

    Delete useless thing

--- src/main/java/com/polylink/usermicroservice/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.polylink.usermicroservice.repositories;
-
-import com.polylink.usermicroservice.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, String> {
-    Role findByName(String name);
-
-}
